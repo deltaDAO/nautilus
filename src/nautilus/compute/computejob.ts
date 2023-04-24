daa5834f9fc19bd973d6f7f16cd6944c522f711f
Moritz Kirstein <mkirstein@protonmail.com>
2023-04-24 13:02:09 +0200

    refactor: remove unused files

--- src/nautilus/compute/computejob.ts
+++
@@ -1,8 +0,0 @@
-import { ComputeAlgorithm, ComputeAsset } from '@oceanprotocol/lib'
-import { AssetWithAccessDetails } from '../../@types/Compute'
-
-export class NautilusComputeJob {
-  dataset: ComputeAsset
-  alogirthm: ComputeAlgorithm
-  additionalDatasets: ComputeAsset[]
-}
