996d653e38c62feb1d53cbbddfe99a3fa074d38e
Moritz Kirstein <mkirstein@protonmail.com>
2023-04-25 20:23:56 +0200

    refactor: checkDocs script

--- scripts/checkDocsVersion.js
+++ scripts/checkDocsVersion.js
@@ -1,9 +1,20 @@
 const fs = require('fs')
 
 const docsDir = './docs/'
-const versionsFile = 'versions.json'
+const versionsFile = `${docsDir}versions.json`
 const versionedDocsDir = `${docsDir}versioned_docs/`
 const versionedSidebarsDir = `${docsDir}versioned_sidebars/`
+
+const colors = {
+  black: `\x1b[30m`,
+  green: `\x1b[32m`,
+  red: `\x1b[31m`
+}
+
+const symbols = {
+  checkmark: `\u2713`,
+  cross: `\u2716`
+}
 
 function checkVersion(version) {
   if (!fs.existsSync(versionsFile)) return handleVersionDoesNotExist(version)
@@ -15,21 +26,25 @@ function checkVersion(version) {
     `${versionedDocsDir}version-${version}`
   )
   const versionedSidebarExists = fs.existsSync(
-    `${versionedSidebarsDir}version-${version}-sidebar.json`
+    `${versionedSidebarsDir}version-${version}-sidebars.json`
   )
 
   if (!versionExists || !versionedDocsExists || !versionedSidebarExists)
     return handleVersionDoesNotExist(version)
 
-  console.log('Docs are ready for release!')
+  process.stdout.write(
+    `${colors.green}${symbols.checkmark} Documentation ready for release.\n`
+  )
 }
 
 function handleVersionDoesNotExist(version) {
   // cross-sign & change color to red
-  process.stderr.write(`\u274C \x1b[31m Documentation not ready for release.`)
+  process.stderr.write(
+    `${colors.red}${symbols.cross} Documentation not ready for release.`
+  )
   // change color to black
   process.stderr.write(
-    `\x1b[30m\n-- The version (${version}) you are trying to release has no docs ready to use.`
+    `${colors.black}\n-- The version (${version}) you are trying to release has no docs ready to use.`
   )
   process.stderr.write(
     '\n-- Make sure to generate the docs first and re-run the release command.'
