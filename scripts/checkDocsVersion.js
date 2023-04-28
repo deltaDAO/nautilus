967109e45494b1c1594319e30ecf8df8d575d993
Moritz Kirstein <mkirstein@protonmail.com>
2023-04-28 21:47:55 +0200

    refactor: check docs version for previews

--- scripts/checkDocsVersion.js
+++ scripts/checkDocsVersion.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const readline = require('readline')
 
 const docsDir = './docs/'
 const versionsFile = `${docsDir}versions.json`
@@ -16,7 +17,52 @@ const symbols = {
   cross: `\u2716`
 }
 
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 function checkVersion(version) {
+  // For releases in active development we only provide one version of docs
+  // If we find a `alpha` or `beta` release, strip the versioning for that preview
+  const earlyPreview = version.includes('beta')
+    ? 'beta'
+    : version.includes('alpha')
+    ? 'alpha'
+    : false
+  if (earlyPreview) {
+    // remove preview specific versioning
+    // 1.0.0-beta.12 becomes 1.0.0-beta
+    version = version.substring(
+      0,
+      version.indexOf(earlyPreview) + earlyPreview.length
+    )
+
+    // Ask user to confirm if they made sure docs are updated
+    // for the early preview version to be released,
+    // since we dont check for multiple versions of the same preview
+    // e.g. 1.0.0-beta.1 uses same version tag as 1.0.0-beta.2 (1.0.0-beta)
+    readlineInterface.question(
+      `Early Preview Detected: ${version}\nDid you update the Docusaurus documentation? (y/n): `,
+      (input) => {
+        if (input !== 'y') {
+          process.stdout.write(`User did not confirm updated documentation.\n`)
+          handleVersionDoesNotExist(version)
+        }
+
+        process.stdout.write(
+          `Checking early preview documentation for ${version}\n`
+        )
+
+        verifyVersionDocsExist(version)
+      }
+    )
+  } else {
+    verifyVersionDocsExist(version)
+  }
+}
+
+function verifyVersionDocsExist(version) {
   if (!fs.existsSync(versionsFile)) return handleVersionDoesNotExist(version)
 
   const versions = JSON.parse(fs.readFileSync(versionsFile))
