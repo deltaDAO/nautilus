43d52cef5331797f561543196556aa5eb52ecd56
Moritz Kirstein <mkirstein@protonmail.com>
2023-05-10 17:10:55 +0200

    chore: cleanup env vars

--- test/fixtures/Web3.ts
+++ test/fixtures/Web3.ts
@@ -5,7 +5,9 @@ dotenv.config()
 
 export function getWeb3(nodeUri?: string) {
   const web3 = new Web3(nodeUri || chainConfig.nodeUri)
-  const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
+  const account = web3.eth.accounts.privateKeyToAccount(
+    process.env.PRIVATE_KEY_TESTS
+  )
   web3.eth.accounts.wallet.add(account)
   web3.defaultAccount = account.address
 
