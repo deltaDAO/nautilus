d41e66bc4a7d6ffb0aff91cc6b61ade7fdb6f32a
Moritz Kirstein <mkirstein@protonmail.com>
2023-08-08 17:37:59 +0200

    test: more ddo unit tests

---
+++ test/fixtures/DDOData.ts
@@ -0,0 +1,5 @@
+export const metadataFixture = {
+  name: 'Name for DDO Testing',
+  description: 'Description for DDO Testing',
+  author: 'Name for DDO Testing'
+}
--- test/unit/NautilusDDO.test.ts
+++ test/unit/NautilusDDO.test.ts
@@ -1,8 +1,9 @@
-import { DDO } from '@oceanprotocol/lib'
+import { DDO, Service } from '@oceanprotocol/lib'
 import assert from 'assert'
 import { FileTypes, NautilusService, ServiceTypes } from '../../src'
 import { NautilusDDO } from '../../src/Nautilus/Asset/NautilusDDO'
 import { datasetService } from '../fixtures/AssetConfig'
+import { metadataFixture } from '../fixtures/DDOData'
 
 describe('NautilusDDO', () => {
   let oceanDDO: DDO
@@ -88,15 +89,8 @@ describe('NautilusDDO', () => {
 
   it('creates a DDO correctly from new service', async () => {
     const nautilusDDO = new NautilusDDO()
-    const service = new NautilusService<ServiceTypes.ACCESS, FileTypes.URL>()
 
-    service.type = datasetService.type as ServiceTypes.ACCESS
-    service.files = datasetService.files as any
-    service.timeout = datasetService.timeout
-    service.datatokenCreateParams = datasetService.datatokenCreateParams
-    service.datatokenAddress = '0x1234datatoken1234Address'
-    service.serviceEndpoint = oceanDDO.services[0].serviceEndpoint
-
+    const service = getNautilusService(oceanDDO.services[0].serviceEndpoint)
     nautilusDDO.services = [service]
 
     const ddo = await nautilusDDO.getDDO(
@@ -107,39 +101,114 @@ describe('NautilusDDO', () => {
 
     const newService = ddo.services[0]
 
-    assert.equal(
-      newService.datatokenAddress,
-      service.datatokenAddress,
-      'The datatokenAddress does not match the input'
-    )
-    assert.equal(
-      newService.type,
-      service.type,
-      'The service type does not match the input'
-    )
-    assert.equal(
-      newService.serviceEndpoint,
-      service.serviceEndpoint,
-      'The serviceEndpoint does not match the input'
-    )
-    assert.equal(
-      newService.timeout,
-      service.timeout,
-      'The timeout value does not match the input'
-    )
-    assert.match(
-      newService.files,
-      /0x.+/,
-      'The created files value does not match the given pattern'
-    )
-    assert(newService.id, 'The new service does not have an id')
+    assertNewDDOService(newService, service)
   })
 
-  it('overwrites previous DDO services correctly', () => {
-    assert(false, 'Test not implemented')
+  it('overwrites previous DDO metadata correctly', async () => {
+    const nautilusDDO = NautilusDDO.createFromDDO(oceanDDO)
+
+    const { name, description, author } = metadataFixture
+
+    nautilusDDO.metadata.name = name
+    nautilusDDO.metadata.description = description
+    nautilusDDO.metadata.author = author
+
+    const ddo = await nautilusDDO.getDDO()
+
+    assert.equal(
+      ddo.metadata.name,
+      name,
+      'metadata.name does not match the input'
+    )
+    assert.equal(
+      ddo.metadata.author,
+      author,
+      'metadata.author does not match the input'
+    )
+    assert.equal(
+      ddo.metadata.description,
+      description,
+      'metadata.description does not match the input'
+    )
+    assert.equal(ddo.id, oceanDDO.id, 'The id does not match the previous id')
   })
 
-  it('overwrites previous DDO metadata correctly', () => {
-    assert(false, 'Test not implemented')
+  it('adds services to existing DDO correctly', async () => {
+    const nautilusDDO = NautilusDDO.createFromDDO(oceanDDO)
+    const service = getNautilusService(oceanDDO.services[0].serviceEndpoint)
+
+    nautilusDDO.services = [service]
+
+    const ddo = await nautilusDDO.getDDO()
+
+    console.log(ddo.services)
+
+    assert(
+      ddo.services.length === 2,
+      'The length of the services array does not match the expected number: 2'
+    )
+
+    const oldServiceIndex = ddo.services.findIndex(
+      (s) => s.id === oceanDDO.services[0].id
+    )
+
+    assert.deepEqual(
+      ddo.services[oldServiceIndex],
+      oceanDDO.services[0],
+      'The previous service does not match and was altered'
+    )
+    assert(
+      ddo.services.length === 2,
+      'The length of the services array does not match the expected number: 2'
+    )
+
+    assertNewDDOService(ddo.services[oldServiceIndex === 0 ? 1 : 0], service)
   })
 })
+
+function assertNewDDOService(
+  actual: Service,
+  expected: NautilusService<ServiceTypes, FileTypes>
+) {
+  assert.equal(
+    actual.datatokenAddress,
+    expected.datatokenAddress,
+    'The datatokenAddress does not match the input'
+  )
+  assert.equal(
+    actual.type,
+    expected.type,
+    'The service type does not match the input'
+  )
+  assert.equal(
+    actual.serviceEndpoint,
+    expected.serviceEndpoint,
+    'The serviceEndpoint does not match the input'
+  )
+  assert.equal(
+    actual.timeout,
+    expected.timeout,
+    'The timeout value does not match the input'
+  )
+  assert.match(
+    actual.files,
+    /0x.+/,
+    'The created files value does not match the given pattern'
+  )
+  assert(actual.id, 'The new service does not have an id')
+}
+
+function getNautilusService(
+  serviceEndpoint: string
+): NautilusService<ServiceTypes, FileTypes> {
+  const service = new NautilusService<ServiceTypes.ACCESS, FileTypes.URL>()
+
+  service.type = datasetService.type as ServiceTypes.ACCESS
+  service.files = datasetService.files as any
+  service.timeout = datasetService.timeout
+  service.datatokenCreateParams = datasetService.datatokenCreateParams
+  service.datatokenAddress = '0x1234datatoken1234Address'
+  service.serviceEndpoint = serviceEndpoint
+
+  return service
+}
