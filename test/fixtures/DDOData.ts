575ccdf8c45b1174eaa9405032e7147f9aa8d581
Moritz Kirstein <mkirstein@protonmail.com>
2023-08-11 15:16:19 +0200

    test: update fixtures

--- test/fixtures/DDOData.ts
+++ test/fixtures/DDOData.ts
@@ -1,5 +1,9 @@
-export const metadataFixture = {
+import { MetadataConfig } from '../../src/@types'
+
+export const metadataFixture: MetadataConfig = {
   name: 'Name for DDO Testing',
   description: 'Description for DDO Testing',
-  author: 'Name for DDO Testing'
+  author: 'Name for DDO Testing',
+  type: 'dataset',
+  license: 'Apache-2.0'
 }
