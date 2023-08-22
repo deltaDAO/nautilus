c988e314d860dc0755b1e44ec7c4f34cc980a860
Moritz Kirstein <mkirstein@protonmail.com>
2023-08-22 17:44:40 +0200

    test: access and compute integration tests

--- test/integration/compute-flow.test.ts
+++ test/integration/compute-flow.test.ts
@@ -18,8 +18,11 @@ import { getTestConfig } from '../fixtures/Config'
 import { nftParams } from '../fixtures/NftCreateData'
 import { getSigner, MUMBAI_NODE_URI } from '../fixtures/Web3'
 import { Signer } from 'ethers'
+import { expect } from 'chai'
 
-describe('Nautilus compute flow integration test', async () => {
+describe('Compute Flow Integration', async function () {
+  this.timeout(90000)
+
   // PRIVATE_KEY_TESTS_1 (algorithm publisher)
   let algoPublisherSigner: Signer
   let algoPublisherAddress: string
@@ -48,7 +51,7 @@ describe('Nautilus compute flow integration test', async () => {
     )
 
     // PRIVATE_KEY_TESTS_2 (dataset publisher)
-    datasetPublisherSigner = getSigner(1, MUMBAI_NODE_URI)
+    datasetPublisherSigner = getSigner(2, MUMBAI_NODE_URI)
     datasetPublisherAddress = await datasetPublisherSigner.getAddress()
 
     nautilusDatasetPublisher = await Nautilus.create(
@@ -90,10 +93,10 @@ describe('Nautilus compute flow integration test', async () => {
     // publish
     const result = await nautilusAlgoPublisher.publish(asset)
 
-    assert(result)
+    expect(result).to.have.property('ddo').to.have.property('id')
 
     computeAlgorithmDid = result.ddo.id
-  }).timeout(30000)
+  })
 
   it('publishes a compute dataset asset', async () => {
     // serviceEndpoint to use for the test asset
@@ -127,40 +130,42 @@ describe('Nautilus compute flow integration test', async () => {
     // publish
     const result = await nautilusDatasetPublisher.publish(asset)
 
-    assert(result)
+    expect(result).to.have.property('ddo').to.have.property('id')
 
     computeDatasetDid = result.ddo.id
-  }).timeout(30000)
+  })
 
   it('starts a compute job', async () => {
     // wait until DDOs are found in metadata cache
     const aquarius = new Aquarius(
       nautilusDatasetPublisher.getOceanConfig().metadataCacheUri
     )
+    console.log(
+      `Waiting for aquarius at ${aquarius.aquariusURL} to cache algo and dataset...`
+    )
     await aquarius.waitForAqua(computeAlgorithmDid)
     await aquarius.waitForAqua(computeDatasetDid)
 
-    assert(false, 'Re-activate test')
+    const startedJob = await nautilusDatasetPublisher.compute({
+      dataset: {
+        did: computeDatasetDid
+      },
+      algorithm: {
+        did: computeAlgorithmDid
+      }
+    })
 
-    // const startedJob = await nautilusDatasetPublisher.compute({
-    //   dataset: {
-    //     did: computeDatasetDid
-    //   },
-    //   algorithm: {
-    //     did: computeAlgorithmDid
-    //   }
-    // })
+    const computeJob = Array.isArray(startedJob) ? startedJob[0] : startedJob
 
-    // const computeJob = Array.isArray(startedJob) ? startedJob[0] : startedJob
+    expect(computeJob).to.have.property('jobId')
 
-    // assert(computeJob)
-
-    // computeJobId = computeJob.jobId
-  }).timeout(90000)
+    computeJobId = computeJob.jobId
+  })
 
   it('should get compute status', async () => {
     const status = await getStatusHelper(nautilusDatasetPublisher, computeJobId)
-    assert(status)
+
+    expect(status).to.have.property('status').to.be.greaterThan(0)
   })
 
   it('should get download url for compute result', async () => {
@@ -168,14 +173,12 @@ describe('Nautilus compute flow integration test', async () => {
 
     const { providerUri } = nautilusDatasetPublisher.getOceanConfig()
 
-    // const resultUrl = await nautilusDatasetPublisher.getComputeResult({
-    //   jobId: computeJobId,
-    //   providerUri
-    // })
+    const resultUrl = await nautilusDatasetPublisher.getComputeResult({
+      jobId: computeJobId,
+      providerUri
+    })
 
-    // assert(resultUrl)
-
-    assert(false, 'Re-activate test')
+    expect(resultUrl).to.match(new RegExp(providerUri))
   })
     // TODO: either increase timeout or introduce different solution to wait for ctd to finish
     // takes longer on live test networks (e.g. Mumbai ~4 minutes)
@@ -185,15 +188,12 @@ describe('Nautilus compute flow integration test', async () => {
 async function getStatusHelper(nautilus: Nautilus, jobId: string) {
   const { providerUri } = nautilus.getOceanConfig()
 
-  // const status = await nautilus.getComputeStatus({
-  //   jobId,
-  //   providerUri
-  // })
+  const status = await nautilus.getComputeStatus({
+    jobId,
+    providerUri
+  })
 
-  // return status
-
-  // TODO: Re-activate helper function
-  return undefined
+  return status
 }
 
 async function waitUntilJobFinishes(
