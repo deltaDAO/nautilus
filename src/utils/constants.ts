4f46325fbf577d6835e99e407cc2915c978e8d5e
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2023-11-24 09:56:09 +0100

    refactor: add AssetSpecificProps type

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,12 @@
+import { Asset, DDO } from '@oceanprotocol/lib'
+
+export type AssetSpecificProps = Exclude<keyof Asset, keyof DDO>
+
 // these are the props which extend the DDO interface to be an aquarius Asset
 // this const is needed to strip these away from an Asset to convert it into an DDO
-export const AQUARIUS_ASSET_EXTENDED_DDO_PROPS = [
+export const AQUARIUS_ASSET_EXTENDED_DDO_PROPS: AssetSpecificProps[] = [
   'nft',
   'datatokens',
-  'event',
   'stats',
   'purgatory'
 ]
