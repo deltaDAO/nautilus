be486ebfb40657e67a9c9b3c98dd70ff44278c82
Moritz Kirstein <mkirstein@protonmail.com>
2024-03-16 00:11:18 +0100

    docs: update og image url

--- docs/vocs.config.tsx
+++ docs/vocs.config.tsx
@@ -25,9 +25,9 @@ export default defineConfig({
   ogImageUrl: {
     '/': '/og-image.png',
     '/docs':
-      'https://vocs.dev/api/og?title=%title&description=%description',
+      'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
     '/op-stack':
-      'https://vocs.dev/api/og?title=%title&description=%description',
+      'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
   },
   iconUrl: { light: '/favicons/light.png', dark: '/favicons/dark.png' },
   logoUrl: { light: '/icon-light.png', dark: '/icon-dark.png' },
