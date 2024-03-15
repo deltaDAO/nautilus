86081a188f46dce7ca4f164b459c9f6b978e12fd
Moritz Kirstein <mkirstein@protonmail.com>
2024-03-15 16:54:58 +0100

    docs: add sponsors

--- docs/vocs.config.tsx
+++ docs/vocs.config.tsx
@@ -39,7 +39,26 @@ export default defineConfig({
       link: 'https://github.com/deltaDAO/nautilus',
     }
   ],
-  sponsors: [],
+  sponsors: [
+    {
+      name: 'Enterprises',
+      height: 80,
+      items: [
+        [
+          {
+            name: 'OceanProtocol',
+            link: 'https://oceanprotocol.com',
+            image: 'https://oceanprotocol.com/static/ae84296f3b9ccb7054530d3af623f1fa/logo.svg',
+          },
+          {
+            name: 'Oasis Network',
+            link: 'https://oasisprotocol.org/',
+            image: 'https://assets-global.website-files.com/63617eb68a66008a1a2130a0/639b268fde36b82f6ca20500_Network%20Logo.svg',
+          }
+        ]
+      ]
+    }
+  ],
   theme: {
     accentColor: {
       light: '#007599',
