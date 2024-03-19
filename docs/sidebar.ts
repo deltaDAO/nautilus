dd24dcde04c0bd94fa82c7700b3f923249ad2ae4
Moritz Kirstein <mkirstein@protonmail.com>
2024-03-19 16:30:49 +0100

    Feat: documentation setup with vocs  (#75)
    * chore(deps): add vocs
    
    * ci: update docs scripts
    
    * docs: remove docusaurus docs
    
    * ci: add vocs config
    
    * docs: add sample page
    
    * docs: setup (WIP)
    
    * chore: add ignore path
    
    * chore: update docs scripts
    
    * chore: switch to bun
    
    * ci: update gitignore
    
    * docs: landing page
    
    * docs: add placeholder pages
    
    * fix: baseUrl
    
    * chore(deps): upgrade typescript
    
    * docs: add favicon
    
    * docs: update overview snippet
    
    * docs: add sponsors
    
    * chore: remove bun lock
    
    * docs: update README header
    
    * docs: update og image url
    
    * chore: audit
    
    * docs: move guides folder
    
    * docs: add getting started placeholder
    
    * ci: remove typedoc workflow
    
    * chore: fix docs crash
    
    * docs: re-add docs examples

---
+++ docs/sidebar.ts
@@ -0,0 +1,24 @@
+import type { Sidebar } from 'vocs'
+
+export const sidebar = {
+  '/docs/': [
+    {
+      text: 'Introduction',
+      items: [
+        { text: 'Why Nautilus', link: '/docs/introduction' },
+        { text: 'Getting Started', link: '/docs/getting-started' },
+        { text: 'FAQ', link: '/docs/faq' }
+      ]
+    },
+    {
+      text: 'Guides',
+      items: [
+        { text: 'Migration Guide', link: '/docs/migration-guide' },
+        { text: 'TypeScript', link: '/docs/typescript' },
+        { text: 'Publishing', link: '/docs/guides/publish' },
+        { text: 'Downloads', link: '/docs/guides/download' },
+        { text: 'Compute to Data', link: '/docs/guides/compute' }
+      ]
+    }
+  ]
+} as const satisfies Sidebar
