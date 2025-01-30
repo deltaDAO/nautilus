7a182e7d81fd25a6ab0c705838e49505c9d42325
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2025-01-30 11:50:34 +0100

    docs: add docs for new nft token setter

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -170,6 +170,11 @@ export const sidebar = {
             },
             { text: 'setLicense', link: '/docs/api/assetbuilder/setLicense' },
             { text: 'setName', link: '/docs/api/assetbuilder/setName' },
+            { text: 'setNftTokenName', link: '/docs/api/assetbuilder/setNftTokenName' },
+            { text: 'setNftTokenSymbol', link: '/docs/api/assetbuilder/setNftTokenSymbol' },
+            { text: 'setNftTokenTemplate', link: '/docs/api/assetbuilder/setNftTokenTemplate' },
+            { text: 'setNftTokenTransferable', link: '/docs/api/assetbuilder/setNftTokenTransferable' },
+            { text: 'setNftTokenUri', link: '/docs/api/assetbuilder/setNftTokenUri' },
             { text: 'setType', link: '/docs/api/assetbuilder/setType' }
           ]
         },
